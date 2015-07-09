fed1b6b47239cdcd9b35835272ad21a6aa199d0b
Matthew Robbins <matthew.robbins@touchstargroup.com>
2015-07-09 17:00:33 +1000

    Add initial source code and project structure.

---
+++ MonoDevelopAddinPackager/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"MonoDevelopAddinPackager", 
+	Namespace = "MonoDevelopAddinPackager",
+	Version = "1.0.0"
+)]
+
+[assembly:AddinName ("MonoDevelop Addin Packager")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("Helper utility that allows monodevelop addin projects to be packaged into an mpack.")]
+[assembly:AddinUrl("https://github.com/matthew-ch-robbins/monodevelop-addin-packager")]
+[assembly:AddinAuthor ("Matthew Robbins")]
