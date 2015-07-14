18d7098e21a1b4fc6e736c1cea2452a046220a24
Matthew Robbins <matthew.robbins@touchstargroup.com>
2015-07-14 14:22:18 +1000

    Preliminary working addin packager.

--- MonoDevelopAddinPackager/PackageProjectHandler.cs
+++ MonoDevelopAddinPackager/PackageProjectHandler.cs
@@ -5,6 +5,9 @@ using MonoDevelop.Ide.Gui;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Projects;
 using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace MonoDevelopAddinPackager
 {
@@ -15,29 +18,53 @@ namespace MonoDevelopAddinPackager
 
 		public readonly string[] MONO_DEVELOP_ADDIN_PROJECT_GUIDS = new string[] {"FAE04EC0-301F-11D3-BF4B-00C04F79EFBC", "86F6BF2A-E449-4B3E-813B-9ACC37E5545F"};
 		public const string PROJECT_GUID_PROP_KEY = "ProjectTypeGuids";
+
+		public const string MONODEVELOP_OSX_INSTALL_PATH = @"/Applications/Xamarin Studio.app/Contents/MacOS/";
+		public const string MONODEVELOP_WINDOWS_INSTALL_PATH = @"C:\Program Files (x86)\Xamarin Studio\bin\";
+
+		public const string MDTOOL_PACK_COMMAND = "setup pack {0} -d:{1}";
 		
 		protected override void Run ()
 		{
 			SolutionItem item = null;
 			CanExecuteInContext (out item);
 
-			var buildResult = item.Build (IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor (), IdeApp.Workspace.ActiveConfiguration);
-			if (buildResult.Failed == false) {
-				var project = item as Project;
-				var outputPath = project.GetOutputFileName (IdeApp.Workspace.ActiveConfiguration);
+			using (var monitor = IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor ()) {
+				var buildResult = item.Build (monitor, IdeApp.Workspace.ActiveConfiguration);
+				if (buildResult.Failed == false) {
+					var project = item as Project;
+					var outputPath = project.GetOutputFileName (IdeApp.Workspace.ActiveConfiguration);
 
-				// Throw this path at out packager.
-				
+					var name = DesktopService.PlatformName;
+
+
+
+					string mdtoolPath = "";
+					if (DesktopService.PlatformName == "OSX") {
+						mdtoolPath = Path.Combine (MONODEVELOP_OSX_INSTALL_PATH, "mdtool");
+					} else {
+						mdtoolPath = Path.Combine (MONODEVELOP_OSX_INSTALL_PATH, "mdtool.exe");
+					}
+
+					ProcessStartInfo startInfo = new ProcessStartInfo();
+					startInfo.Arguments = String.Format (MDTOOL_PACK_COMMAND, outputPath.ToString(), outputPath.ParentDirectory.ToString ());
+					startInfo.UseShellExecute = false;
+					startInfo.RedirectStandardOutput = true;
+					startInfo.FileName = mdtoolPath;
+
+					// TODO: Redirect out 
+
+					Process process = new Process ();
+					process.StartInfo = startInfo;
+					process.OutputDataReceived += (object sender, DataReceivedEventArgs e) => IdeApp.Workbench.StatusBar.ShowMessage (e.Data);
+					process.Start ();
+
+					if (process.WaitForExit (60000)) {
+						DesktopService.OpenFolder (outputPath.ParentDirectory.ToString ());
+					} else {
+					}
+				}
 			}
-
-//			if (CanExecuteInContext (out item)) {
-//				var buildResult = item.Build (IdeApp.Workbench.ProgressMonitors.GetBuildProgressMonitor (), IdeApp.Workspace.ActiveConfiguration);
-//				if (buildResult.Failed == false) {
-//					// Get configuratyion output and execute the mdtool command.
-//					buildResult.
-//
-//				}
-//			}
 		}
 
 		protected bool CanExecuteInContext(out SolutionItem targettedItem)
@@ -85,9 +112,12 @@ namespace MonoDevelopAddinPackager
 
 		protected override void Update (CommandInfo info)
 		{
+			var binPath = Path.GetDirectoryName(Assembly.GetEntryAssembly ().Location);
+
+			var files = Directory.GetFiles (binPath);
+
 			SolutionItem item = null;
-			//info.Enabled = CanExecuteInContext (out item);
-			info.Enabled = true;
+			info.Enabled = CanExecuteInContext (out item);
 		}   
 	}
 }
