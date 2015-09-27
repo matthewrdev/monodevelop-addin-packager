297659f6ecc5b43aa917bd7e0c138da0451f1703
Matthew Robbins <mrobbins_99@hotmail.com>
2015-09-27 18:11:14 +1000

    Update assembly description

--- MonoDevelopAddinPackager/Properties/AddinInfo.cs
+++ MonoDevelopAddinPackager/Properties/AddinInfo.cs
@@ -10,6 +10,6 @@ using Mono.Addins.Description;
 
 [assembly:AddinName ("MonoDevelop Addin Packager")]
 [assembly:AddinCategory ("IDE extensions")]
-[assembly:AddinDescription ("Utility to package and deploy MonoDevelop addins. Adds 'Package Addin' and 'Create Addin Web Index options to the Project menu.")]
+[assembly:AddinDescription ("Tooling to assist the development of MonoDevelop addins.\n\nCurrent features:\n\n - 'Package Addin' to pack a MonoDevelop Addin project into an .mpack.\n - 'Create Addin Web Index (.mrep)' to build web index .mrep files for the addin.\n - 'Clean Addin Packages' to delete all generated package files for the active project and configuration.")]
 [assembly:AddinUrl("https://github.com/matthew-ch-robbins/monodevelop-addin-packager")]
 [assembly:AddinAuthor ("Matthew Robbins")]
